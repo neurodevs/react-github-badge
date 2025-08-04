7543ff26747a9d6c4001c37dc091fe46a11bb460
ericthecurious <hello@ericthecurious.com>
2025-08-03 18:20:16 -0700

    minor: create FakeTestCounterBadge

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { default as TestCounterBadge } from './ui/TestCounterBadge'
 export * from './ui/TestCounterBadge'
+
+export { default as FakeTestCounterBadge } from './testDoubles/FakeTestCounterBadge'
+export * from './testDoubles/FakeTestCounterBadge'
---
+++ src/testDoubles/FakeTestCounterBadge.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TestCounterBadgeProps } from '../ui/TestCounterBadge'
+
+export let lastFakeTestCounterBadgeProps: TestCounterBadgeProps | undefined
+
+const FakeTestCounterBadge: React.FC<TestCounterBadgeProps> = (
+    props: TestCounterBadgeProps
+) => {
+    lastFakeTestCounterBadgeProps = props
+
+    return (
+        <div className="test-counter-badge" data-testid="test-counter-badge" />
+    )
+}
+
+export default FakeTestCounterBadge
