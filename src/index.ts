838d91d2da05b187d41936c4fb35c2bf7b390199
ericthecurious <hello@ericthecurious.com>
2025-08-03 19:25:58 -0700

    patch: change export strategy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { default as TestCounterBadge } from './ui/TestCounterBadge'
 export * from './ui/TestCounterBadge'
 
-export { default as FakeTestCounterBadge } from './testDoubles/FakeTestCounterBadge'
 export * from './testDoubles/FakeTestCounterBadge'
